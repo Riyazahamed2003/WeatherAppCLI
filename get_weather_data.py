9a700bfd1f932c6b178871dbdebf3dd2c332b481
Riyaz Ahamed K K <118614045+Riyazahamed2003@users.noreply.github.com>
2023-08-02 12:28:57 +0530

    Add get_weather_data function
    This file contains the code for fetching weather data from OpenWeatherMap.

---
+++ get_weather_data.py
@@ -0,0 +1,6 @@
+import requests
+
+def get_weather_data():
+    url = "https://samples.openweathermap.org/data/2.5/forecast/hourly?q=London,us&appid=b6907d289e10d714a6e88b30761fae22"
+    response = requests.get(url)
+    return response.json()
